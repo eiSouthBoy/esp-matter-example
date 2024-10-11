607dbdf8f401d0ec03da84e369bfba0eac0c03ba
eiSouthBoy <caoj97141@gmail.com>
2024-10-11 10:11:35 +0800

    add plug-in

---
+++ plug-in/main/app_driver.cpp
@@ -0,0 +1,131 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_log.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <esp_matter.h>
+#include "bsp/esp-bsp.h"
+
+#include <app_priv.h>
+
+using namespace chip::app::Clusters;
+using namespace esp_matter;
+
+static const char *TAG = "app_driver";
+extern uint16_t g_onoff_plugin_endpoint_id;
+
+// LED Power Set
+/* Do any conversions/remapping for the actual value here */
+static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+#if CONFIG_BSP_LEDS_NUM > 0
+    esp_err_t err = ESP_OK;
+    ESP_LOGI(TAG, "LED set power: %d", val->val.b);
+    if (val->val.b) 
+    {
+        err = led_indicator_start(handle, BSP_LED_ON);
+    } 
+    else 
+    {
+        err = led_indicator_start(handle, BSP_LED_OFF);
+    }
+    return err;
+#else
+    ESP_LOGI(TAG, "LED set power: %d", val->val.b);
+    return ESP_OK;
+#endif
+}
+
+// Button Toggle(切换，反转)
+static void app_driver_button_toggle_cb(void *arg, void *data)
+{
+    ESP_LOGI(TAG, "Toggle button pressed");
+    uint16_t endpoint_id = g_onoff_plugin_endpoint_id;
+    uint32_t cluster_id = OnOff::Id;
+    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
+
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = cluster::get(endpoint, cluster_id);
+    attribute_t *attribute = attribute::get(cluster, attribute_id);
+
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+    attribute::get_val(attribute, &val);
+    val.val.b = !val.val.b;
+    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
+}
+
+// Attribute(属性) Update
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val)
+{
+    esp_err_t err = ESP_OK;
+  
+    if (endpoint_id == g_onoff_plugin_endpoint_id)
+    {
+        led_indicator_handle_t handle = (led_indicator_handle_t)driver_handle;
+        if (cluster_id == OnOff::Id)
+        {
+            if (attribute_id == OnOff::Attributes::OnOff::Id)
+            {
+                err = app_driver_light_set_power(handle, val);
+            }
+        }
+    }
+    return err;
+}
+
+esp_err_t app_driver_onoff_plugin_set_defaults(uint16_t endpoint_id)
+{
+    esp_err_t err = ESP_OK;
+    void *priv_data = endpoint::get_priv_data(endpoint_id);
+    led_indicator_handle_t handle = (led_indicator_handle_t)priv_data;
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = NULL;
+    attribute_t *attribute = NULL;
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+
+    /* Setting power */
+    cluster = cluster::get(endpoint, OnOff::Id);
+    attribute = attribute::get(cluster, OnOff::Attributes::OnOff::Id);
+    attribute::get_val(attribute, &val);
+    err |= app_driver_light_set_power(handle, &val);
+
+    return err;
+}
+
+/* LED初始化 */
+app_driver_handle_t app_driver_light_init()
+{
+#if CONFIG_BSP_LEDS_NUM > 0
+    /* Initialize led */
+    led_indicator_handle_t leds[CONFIG_BSP_LEDS_NUM];
+    ESP_ERROR_CHECK(bsp_led_indicator_create(leds, NULL, CONFIG_BSP_LEDS_NUM));
+    led_indicator_set_hsv(leds[0], SET_HSV(DEFAULT_HUE, DEFAULT_SATURATION, DEFAULT_BRIGHTNESS));
+    // led_indicator_start(leds[0], true);
+
+    return (app_driver_handle_t)leds[0];
+#else
+    return NULL;
+#endif
+}
+
+/* 按键初始化 */
+app_driver_handle_t app_driver_button_init()
+{
+    /* Initialize button */
+    button_handle_t btns[BSP_BUTTON_NUM];
+    ESP_ERROR_CHECK(bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM));
+    ESP_ERROR_CHECK(iot_button_register_cb(btns[0], BUTTON_PRESS_DOWN, app_driver_button_toggle_cb, NULL));
+    
+    return (app_driver_handle_t)btns[0];
+}
